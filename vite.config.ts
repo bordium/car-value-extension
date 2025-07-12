30847aa1404af686ebdd00af6e254a1d007f904c
Stephen Huang <huang.stephen05@gmail.com>
2025-07-12 15:16:22 -0700

    Setup options page and build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        options: resolve(__dirname, 'src/options/index.html'),
+      },
+      output: {
+        dir: "dist"
+      }
+    }
+  }
 })
